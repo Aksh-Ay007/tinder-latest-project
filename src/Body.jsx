aed292fff961d08f2aed11c3730394cae376b177
Akshay Jyothi.P <akshayjyothip@gmail.com>
2025-03-21 22:23:56 +0530

    cant access other without login

--- src/Body.jsx
+++ src/Body.jsx
@@ -1,9 +1,41 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import Navbar from './Navbar';
 import { Outlet } from 'react-router-dom';
 import Footer from './Footer';
+import axios from 'axios';
+import { BASE_URL } from "./utils/constants";
+import { useDispatch, useSelector } from 'react-redux';
+import { addUser } from "./utils/userSlice";
+import { useNavigate } from "react-router-dom";
 
 function Body() {
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const userData=useSelector((store)=>store.user);
+  
+  const fetchuser = async () => {
+    
+    if(userData)return;
+    try {
+      const res = await axios.get(BASE_URL + "/profile/view", { withCredentials: true });
+      dispatch(addUser(res.data));
+      console.log(res.data);
+    } catch (error) {
+      
+      if(error.status===401){
+        navigate('/login');
+      }
+
+      console.error("Login failed:", error);
+    }
+  };
+
+  useEffect(() => {
+
+    
+    fetchuser();
+  }, []); // Add dependency array to ensure useEffect runs only once
+
   return (
     <div className="flex flex-col min-h-screen">
       <Navbar />
--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -1,28 +1,43 @@
 import React from "react";
 import { useSelector } from "react-redux";
+import { Link } from "react-router-dom";
 
 function Navbar() {
   const user = useSelector((store) => store.user);
-  console.log(user, 'navv');
 
   return (
     <nav className="fixed top-0 left-0 w-full z-50 bg-gradient-to-r from-pink-500 to-red-500 text-white shadow-lg px-4 py-2 flex justify-between items-center">
       {/* Logo */}
-      <a className="text-xl sm:text-2xl font-bold flex items-center gap-2 bg-white text-pink-600 px-4 py-1 rounded-full shadow-md hover:bg-red-100 transition">
-        ❤️ TinderVibe 😉🔥
-      </a>
+      <Link to={'/'} className="text-xl sm:text-2xl font-bold flex items-center gap-2 bg-white text-pink-600 px-4 py-1 rounded-full shadow-md hover:bg-red-100 transition">
+        Bondify ❤️
+      </Link>
 
       {/* Profile Section */}
       {user && (
         <div className="flex items-center gap-4">
           <p className="text-lg">Welcome, {user.firstName}</p>
-          <button className="flex items-center justify-center w-12 h-12 rounded-full border-2 border-white focus:outline-none">
-            <img
-              className="w-full h-full rounded-full object-cover"
-              src={user.photoUrl}
-              alt="User photo"
-            />
-          </button>
+          <div className="dropdown dropdown-end">
+            <div tabIndex={0} role="button" className="btn btn-ghost btn-circle avatar">
+              <div className="w-10 rounded-full">
+                <img
+                  alt="User photo"
+                  src={user.photoUrl}
+                />
+              </div>
+            </div>
+            <ul
+              tabIndex={0}
+              className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow">
+              <li>
+                <Link to={'/profile'} className="justify-between">
+                  Profile
+                  <span className="badge">New</span>
+                </Link>
+              </li>
+              <li><Link>Settings</Link></li>
+              <li><Link to={'/logout'}>Logout</Link></li>
+            </ul>
+          </div>
         </div>
       )}
     </nav>
